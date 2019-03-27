20f34b0839f2c92639ef978555eeca2af9187bfb
Ititi Obidah <iobidah9@gmail.com>
2019-03-27 16:35:57 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -60,10 +60,10 @@ def runner
   welcome
  hand = initial_round
 
-  until hand > 21 do 
-  hit?(hand)
-  display_card_total
+ until hand > 21 do 
+ hand = hit?(hand)
+ display_card_total(hand)
 end
-  end_game
+  end_game(hand)
 end
     
